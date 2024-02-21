c8c55357324ef019765ebffe378d500da5e9bf13
shixiaobo <sxb5626@163.com>
2024-02-21 21:14:23 +0800

    feat: 123

---
+++ simple-react/scripts/rollup/react.config.js
@@ -0,0 +1,51 @@
+import { resolvePkgPath, getPackageJSON, getBaseRollupPlugins } from './utils';
+import geneatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJSON('react');
+// react包路径
+const pkgPath = resolvePkgPath(name);
+// react产物路径
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'index.js',
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollupPlugins(),
+			geneatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-rutime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: getBaseRollupPlugins()
+	}
+];
